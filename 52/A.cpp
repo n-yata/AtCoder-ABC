a0f1071ea5bbd2c2978efc4fa588093c67ad411f
n-yata <yata1214@gmail.com>
2021-05-04 17:05:04 +0900

    5/4 3

---
+++ 52/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << max(a*b, c*d) << endl;
+    return 0;
+}
